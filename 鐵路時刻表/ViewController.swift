8000561efb0de3a4d1fa37a5cb6a5f377293aa1e
黃暉德 <hueiderhuang@hueiderhuangmacbookair.local>
2019-01-08 23:16:12 +0800

    Initial Commit

---
+++ 鐵路時刻表/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  鐵路時刻表
+//
+//  Created by 黃暉德 on 2019/1/8.
+//  Copyright © 2019 黃暉德. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
